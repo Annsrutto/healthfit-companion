192ec5ae4eb6d1bd6fed4da3bffd8aad26aaf0ee
Annsrutto <annsrutto567@gmail.com>
2024-08-15 02:34:43 +0300

    Setup environment

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <h1>Fitness Tracker App</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, quaerat?</p>
+    </React.StrictMode>
+)
