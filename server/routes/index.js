45812f8401c5624ae6623b06d635221b72daea95
davykoch <koechdavis24@gmail.com>
2024-08-19 01:17:47 +0300

    routes setup and application

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,12 @@
-import express from 'express'
+import express from 'express';
+import authRoutes from './auth.js';
+import exerciseRoutes from './exercises.js';
+import goalRoutes from './goals.js';
 
 const router = express.Router;
+
+router.use('/auth', authRoutes);
+router.use('/exercises', exerciseRoutes);
+router.use('./goals', goalRoutes);
 
 export default router;
