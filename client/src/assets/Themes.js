4e34f1be5c25974d374449121cc7f6043ac7c7eb
Annsrutto <annsrutto567@gmail.com>
2024-08-19 03:38:47 +0300

    Setting up Authentication

--- client/src/assets/Themes.js
+++ client/src/assets/Themes.js
@@ -1,7 +1,7 @@
 export const lightTheme = {
     bg: "#FFFFFF",
     bgLight: "#FFFFFF",
-    primary: "#007AFF",
+    primary: "#32e935",
     secondary: "#5B86E5",
   
     disabled: "#b1b2b3",
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import TextInput from './TextInput';
+import Button from './Button';
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    gap: 36px;
+`;
+
+const Title = styled.div`
+font-size: 30px;
+font-weight: 800;
+color: ${({ theme }) => theme.text_primary};
+`;
+
+const Span = styled.span``;
+
+const Register = () => {
+  return (
+    <Container>
+        <div>
+            <Title>Welcome to AfyaFit!👋</Title>
+            <Span>Please enter your login details here</Span>
+        </div>
+        <div style={{
+            display: "flex",
+            gap: "20px",
+            flexDirection: "column",
+        }}
+        >
+            <TextInput label="Full Name" placeholder="Enter your full name"/>
+            <TextInput label="Email Address" placeholder="Enter your email address"/>
+            <TextInput label="Password" placeholder="Enter your password"/>
+            <Button text="Register" />
+        </div>
+    </Container>
+  )
+}
+
+export default Register;
