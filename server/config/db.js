f6163dee896330ac51d2bb6f5bd8223e8309a28f
davykoch <koechdavis24@gmail.com>
2024-08-21 00:50:22 +0300

    DB connection setup

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			userNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true
+		});
+		console.log('Connected to MongoDB');
+	} catch (err) {
+		console.error('Could not connect to MongoDB:', err.message);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes/index.js';
+import connectDB from './config/db.js';
 import errorHandler from './middleware/error.js';
 
 dotenv.config();
@@ -18,14 +18,7 @@ app.use(errorHandler);
 app.use('/api', routes);
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true 
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+connectDB();
 
  // Start the server
 const PORT = process.env.PORT || 5000;
