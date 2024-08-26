67ae886294f88821da0c2ef41b667a5f1aa8db49
davykoch <koechdavis24@gmail.com>
2024-08-26 22:41:21 +0300

    port change

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use('/api/goals', goalRoutes);
 connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
